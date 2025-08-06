a22e4774238e24169b4d87bba44072ef7d3e6c35
Devin Mounts <devinmounts@gmail.com>
2025-08-05 23:10:50 -0700

    run R file through appendix, failed on searching for stata robustness files.

--- R_files/ercot_appendix_robustness.R
+++ R_files/ercot_appendix_robustness.R
@@ -1,7 +1,7 @@
 ##### packages 
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load('tidyverse', 'ggplot2', 'readxl', 'janitor', 'lubridate', 'geojsonio', 'broom', 'sp', 'zoo', 'fastDummies', 'stargazer', 'RStata', 'suncalc',
-               'cowplot', 'olsrr', 'sf', 'MESS', 'pryr', 'plyr')
+               'cowplot', 'olsrr', 'sf', 'MESS', 'dplyr')
 
 
 ### necessary scripts
@@ -10,39 +10,40 @@ source('eia_pre_process_functions.R')
 source('statistic_functions.R')
 source('regression_script.R')
 source('figure_and_stat_functions.R')
+source('variable_layers_regression_script.R')
 
-#######################################################################
-#######################################################################
-################ Appendix Figures and Tables ##########################
-#######################################################################
-#######################################################################
-
-### Summary Statistics for capacity model result from functions in main body section
-
-### Profits of Major Firms and significance of scarcity adders
-print('Plotting Profit Margin of Major Firms')
-plot_profit_margin_select_firms()
-
-### Full capacity model and applicant pool results tables produced in main body section
-
-#######################################################################################
-################### Underbidding Autoregressive Robustness ############################
-######################################################################################
-run_polynomial_weather=TRUE
-print('running rls ar1 underbidding model')
-run_rls_ar1_timeseries_underbidding_model(run_polynomial_weather)
-print('running rls ar10 underbidding model')
-run_rls_ar10_timeseries_underbidding_model(run_polynomial_weather)
-
-
-#######################################################################################
-################### Capacity Model Robustness ############################
-######################################################################################
-print('running variable lags test')
-run_variable_lags_test()
-print('create variable lags appendix and summary figure')
-create_variable_lags_appendix_summary_and_figure()
-gc()
+# #######################################################################
+# #######################################################################
+# ################ Appendix Figures and Tables ##########################
+# #######################################################################
+# #######################################################################
+# 
+# ### Summary Statistics for capacity model result from functions in main body section
+# 
+# ### Profits of Major Firms and significance of scarcity adders
+# print('Plotting Profit Margin of Major Firms')
+# plot_profit_margin_select_firms()
+# 
+# ### Full capacity model and applicant pool results tables produced in main body section
+# 
+# #######################################################################################
+# ################### Underbidding Autoregressive Robustness ############################
+# ######################################################################################
+# run_polynomial_weather=TRUE
+# print('running rls ar1 underbidding model')
+# run_rls_ar1_timeseries_underbidding_model(run_polynomial_weather)
+# print('running rls ar10 underbidding model')
+# run_rls_ar10_timeseries_underbidding_model(run_polynomial_weather)
+# 
+# 
+# #######################################################################################
+# ################### Capacity Model Robustness ############################
+# ######################################################################################
+# print('running variable lags test')
+# run_variable_lags_test()
+# print('create variable lags appendix and summary figure')
+# create_variable_lags_appendix_summary_and_figure()
+# gc()
 
 print('run variable layers regression test')
 run_variable_layers_regression_test()
